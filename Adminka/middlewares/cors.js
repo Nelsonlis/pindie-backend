4ae7bb82bcec5cc66e8ef3f308c67d92d808daad
Nelsonlis <149626479+Nelsonlis@users.noreply.github.com>
2024-05-29 18:59:35 +0400

    <сообщение коммита>

--- Adminka/middlewares/cors.js
+++ Adminka/middlewares/cors.js
@@ -1,20 +1,14 @@
+const allowedCors = ["https://pengvinzxc.nomorepartiesco.ru"];
+
 function cors(req, res, next) {
-    const { origin } = req.headers
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    }
-    next()
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000',
-    'http://localhost:3000',
-    'localhost:3002',
-    'https://sirdebar.nomoredomainswork.ru'
-  ];
-
-  module.exports = cors
+module.exports = cors;
